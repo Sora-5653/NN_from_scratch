c8408654030970c13a98a77602f0d366909c4151
Seiya5653 <115401160+Sora-5653@users.noreply.github.com>
2023-10-10 10:00:52 +0000

    committed

--- main.py
+++ main.py
@@ -101,4 +101,51 @@ def get_accuracy_value(Y_hat, Y):
 #↓コピペ
 
 #oct. 9。やっぱ休憩。
-#could commit with ssh! brilliant!!
+def single_layer_backward_propagation(dA_curr, W_curr, b_curr, Z_curr, A_prev, activation="relu"):
+    m = A_prev.shape[1]
+    
+    if activation is "relu":
+        backward_activation_func = relu_backward
+    elif activation is "sigmoid":
+        backward_activation_func = sigmoid_backward
+    else:
+        raise Exception('Non-supported activation function')
+    
+    dZ_curr = backward_activation_func(dA_curr, Z_curr)
+    dW_curr = np.dot(dZ_curr, A_prev.T) / m
+    db_curr = np.sum(dZ_curr, axis=1, keepdims=True) / m
+    dA_prev = np.dot(W_curr.T, dZ_curr)
+
+    return dA_prev, dW_curr, db_curr
+
+def full_backward_propagation(Y_hat, Y, memory, params_values, nn_architecture):
+    grads_values = {}
+    m = Y.shape[1]
+    Y = Y.reshape(Y_hat.shape)
+   
+    dA_prev = - (np.divide(Y, Y_hat) - np.divide(1 - Y, 1 - Y_hat));
+    
+    for layer_idx_prev, layer in reversed(list(enumerate(nn_architecture))):
+        layer_idx_curr = layer_idx_prev + 1
+        activ_function_curr = layer["activation"]
+        
+        dA_curr = dA_prev
+        
+        A_prev = memory["A" + str(layer_idx_prev)]
+        Z_curr = memory["Z" + str(layer_idx_curr)]
+        W_curr = params_values["W" + str(layer_idx_curr)]
+        b_curr = params_values["b" + str(layer_idx_curr)]
+        
+        dA_prev, dW_curr, db_curr = single_layer_backward_propagation(
+            dA_curr, W_curr, b_curr, Z_curr, A_prev, activ_function_curr)
+        
+        grads_values["dW" + str(layer_idx_curr)] = dW_curr
+        grads_values["db" + str(layer_idx_curr)] = db_curr
+    
+    return grads_values
+
+#ここで本命のアップデートを行う関数
+def update(parms_values, grads_values, nn_architecture, learning_rate):
+    for layer_idx, layer in enumerate(nn_architecture):
+        parms_values["W" + str(layer_idx)]
+        parms_values["b" + str()]
